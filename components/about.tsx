bdcbed20b20d4275f9886bfdb419fe09bcfba0ac
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-07-28 18:59:50 +0530

    about section done

---
+++ components/about.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <section id="about" className="custom-container">
+            <div className="flex flex-col items-start gap-8">
+                <h2 className="section-header">About me</h2>
+                <p className="text-gray-300 sm:text-lg">
+                    Hi, I'm Sachintha Prasad, an undergraduate in Computing and
+                    Information Systems at Sabaragamuwa University of Sri Lanka.
+                    I am passionate about full-stack development and possess a
+                    strong ability to quickly learn and adapt to new technology
+                    stacks. With excellent problem-solving and interpersonal
+                    skills, I thrive under pressure and continuously seek
+                    opportunities for professional growth and development. I
+                    have experience in both front-end and back-end development,
+                    with a keen interest in building dynamic web applications.
+                    Currently, I am working on projects that involve React,
+                    Tailwind CSS, and Spring Boot. Let's connect and explore
+                    opportunities to build innovative solutions together.
+                </p>
+            </div>
+        </section>
+    )
+}
