76ab699e93f2d9895c65b7d261ff55ffa4560a31
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-07-29 10:01:22 +0530

    vercel speed insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Rubik } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/header'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const rubik = Rubik({
     weight: ['400', '500', '600', '700', '800', '900'],
@@ -30,6 +31,7 @@ export default function RootLayout({
                 </div>
                 <Header />
                 {children}
+                <SpeedInsights />
             </body>
         </html>
     )
